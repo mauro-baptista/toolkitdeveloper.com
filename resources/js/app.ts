930e156b7e8565c70d0bc19fa6554552d1c0536c
Mauro Baptista <mauro@carnou.com>
2022-11-29 17:20:44 -0300

    added origin for cors

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,8 +4,7 @@ import '../css/app.css';
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-
-//import { InertiaProgress } from '@inertiajs/progress'
+import { InertiaProgress } from '@inertiajs/progress'
 
 createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
@@ -15,5 +14,4 @@ createInertiaApp({
             .mount(el)
     },
 });
-
-// InertiaProgress.init()
+InertiaProgress.init()
